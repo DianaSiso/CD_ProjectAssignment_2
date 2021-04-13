d0ea4955ad83d0b81b6f982b112ac33613fc8e17
Diana Siso <diana.siso@ua.pt>
2021-04-13 09:57:22 +0100

    utils.py atualizado

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -5,7 +5,7 @@ from utils import contains_predecessor, contains_successor
 def test_contains_predecessor():
     assert contains_predecessor(300, 100, 200)
     assert contains_predecessor(100, 200, 300)
-    assert not contains_predecessor(300, 400, 100)
+    assert contains_predecessor(300, 400, 100)      #alterei, antes estavam um não
 
 def test_contains_successor():
     assert contains_successor(100, 300, 200)
